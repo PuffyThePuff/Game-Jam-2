fc233410e26f355e14d64b9213161336d3691a91
PuffyThePuff <79883134+PuffyThePuff@users.noreply.github.com>
2021-09-07 15:58:33 +0800

    Added spotlight that follows mouse position

---
+++ Assets/Scripts/PointAndClickController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PointAndClickController : MonoBehaviour
+{
+    public Camera cam;
+
+    [SerializeField] private Transform spotlight;
+
+    [SerializeField] private List<GameObject> placeableObjects;
+
+    private void Start()
+    {
+        spotlight = this.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //shoot a ray from the cam to mouse position
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        //returns true if ray hits an object
+        if (Physics.Raycast(ray, out hit))
+        {
+            spotlight.position = new Vector3(hit.point.x, spotlight.position.y, hit.point.z);
+        }
+    }
+}
