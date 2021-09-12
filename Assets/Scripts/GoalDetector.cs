44ea8dff83b4424be57c3696981d195032be29ff
PuffyThePuff <79883134+PuffyThePuff@users.noreply.github.com>
2021-09-12 17:37:22 +0800

    Ants now ignore other ants

--- Assets/Scripts/GoalDetector.cs
+++ Assets/Scripts/GoalDetector.cs
@@ -1,17 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GoalDetector : MonoBehaviour
 {
+    private Text text;
+    [SerializeField] private int antMax;
+    private int antCurrent;
+
     // Start is called before the first frame update
     void Start()
     {
+        text = GetComponent<Text>();
+        antCurrent = antMax;
+        string newText;
+        newText = "Ants: " + antCurrent + "/" + antMax;
+        text.text = newText;
         GameEvents.current.onAntReachGoal += OnAntReachGoal;
     }
 
     private void OnAntReachGoal()
     {
-        //update UI here
+        antCurrent--;
+        string newText;
+        newText = "Ants: " + antCurrent + "/" + antMax;
+        text.text = newText;
     }
 }
